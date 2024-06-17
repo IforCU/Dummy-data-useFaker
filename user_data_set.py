e038274defb81a295f59ec552792c213ce93292f
IforCU <tmdgus0844@gmail.com>
2024-06-17 22:32:56 +0900

    Feat: User date를 위한 set을 추가

---
+++ user_data_set.py
@@ -0,0 +1,46 @@
+import random
+import openpyxl
+from faker import Faker
+import openpyxl.workbook
+from faker.providers import DynamicProvider
+
+# Faker 라이브러리를 지정함.
+fake = Faker()
+
+# openpyxl를 이용해 Workbook 생성
+workbook = openpyxl.Workbook()
+
+# Workbook의 Sheet를 지정함
+sheet = workbook.active
+
+# column 값 설정
+# 예시 id, 이름, 이미지, 역할, 슈퍼호스트, 호스팅 날짜, 가입날짜, 국적, 내위치, 전화번호, 이메일 등등
+columns = ["id", "name", "image", "roll", "superhost", "host_since", "created_at", "country", "location", "phone_number", "email"]
+sheet.append(columns)
+
+# 1000개의 데이터 셋 생성
+for i in range(1, 1001):
+    # 호스팅 날짜는 가입 날짜보다 빠르면 안된다.
+    signup_date = fake.date_between(start_date='-20y', end_date='today')
+    hosting_date = fake.date_between(start_date=signup_date, end_date='today') if random.choice([True, False]) else None
+        # 호스팅 날짜가 없으면 user 있으면 host가 되어야 함.
+    role = "HOST" if hosting_date else "USER"
+    superhost = fake.boolean() if role == "HOST" else False
+    
+    row = [
+        i,  # ID
+        fake.name(),  # 이름
+        fake.image_url(),  # 이미지
+        role,  # 역할
+        superhost,  # 슈퍼호스트
+        hosting_date.isoformat() if hosting_date else "N/A",  # 호스팅 날짜
+        signup_date.isoformat(),  # 가입 날짜
+        fake.country(),  # 국적
+        fake.city(),  # 내 위치
+        fake.phone_number(),  # 전화번호
+        fake.email()  # 이메일
+    ]
+    sheet.append(row)
+
+# 엑셀 파일 저장
+workbook.save("dummy_data.xlsx")
